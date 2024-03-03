86d4761885cf2c44a25afebcb7fc079d50902fa8
Junho Yeo <i@junho.io>
2024-03-04 02:03:34 +0900

    `RequestPayParams` 에 신규 파라미터 `escrowProducts`, `vat_amount`, `confirm_url` 추가

--- src/RequestPayParams.ts
+++ src/RequestPayParams.ts
@@ -18,13 +18,39 @@ export interface RequestPayParams extends RequestPayAdditionalParams {
   pg?: Pg;
   pay_method?: PaymentMethod;
   escrow?: boolean;
+
+  /**
+   * `escrowProducts`: 에스크로 결제 정보
+   * 에스크로 결제(escrow: true)시에만 유효하고, 필수 값은 아닙니다.
+   * 토스페이먼츠 신모듈 (pg: tosspayments.~)시에만 유효합니다.
+   * 1개의 주문 건에 여러 상품이 결제될 때 상품에 따라 배송이 다르게 이루어지는 경우, 1개의 주문에 여러 배송 정보를 넣기 위해 필요합니다.
+   */
+  escrowProducts?: {
+    id: string;
+    name: string;
+    code: string;
+    unitPrice: number;
+    quantity: number;
+  }[];
+
   customer_uid?: string;
   merchant_uid: string;
   name?: string;
   amount: number;
   custom_data?: any;
   tax_free?: number;
+
+  /**
+   * @deprecated `vat` 대신 `vat_amount` 를 사용해 주세요.
+   */
   vat?: number;
+
+  /**
+   * `vat_amount` (number | null): 부가세
+   * 결제 금액 중 부가세(기본값: null)
+   * 지원되는 PG사: 나이스페이먼츠, (신) 토스페이
+   */
+  vat_amount?: number | null;
   currency?: string;
   language?: string;
   buyer_email?: string;
@@ -32,6 +58,8 @@ export interface RequestPayParams extends RequestPayAdditionalParams {
   buyer_tel: string;
   buyer_addr?: string;
   buyer_postcode?: string;
+
+  confirm_url?: string;
   notice_url?: string | string[];
   display?: Display;
 }
