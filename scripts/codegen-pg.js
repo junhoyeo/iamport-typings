2c8225cce7163920efed58a70e94f95a04162c37
Junho Yeo <i@junho.io>
2024-03-04 01:28:10 +0900

    `Pg` 업데이트 및 Codegen 작성

---
+++ scripts/codegen-pg.js
@@ -0,0 +1,79 @@
+const fs = require('fs');
+const path = require('path');
+const prettier = require('prettier');
+
+// https://developers.portone.io/docs/ko/sdk/javascript-sdk/payrq?v=v1#%EA%B2%B0%EC%A0%9C%EC%9A%94%EC%B2%AD-%ED%8C%8C%EB%9D%BC%EB%AF%B8%ED%84%B0-%EC%A0%95%EC%9D%98
+const LATEST_PORTONE = `
+html5_inicis(이니시스웹표준)
+inicis_unified(이니시스 통합인증)
+inicis(이니시스ActiveX결제창 or API 방식 )
+kcp(NHN KCP 일반결제창 or API 방식 정기결제)
+kcp_billing(NHN KCP 정기결제)
+uplus(토스페이먼츠(구 LG U+))
+tosspayments((신)토스페이먼츠)
+nice(나이스페이)
+nice_v2((신)나이스페이)
+jtnet(JTNet)
+kicc(한국정보통신)
+bluewalnut(블루월넛)
+kakaopay(카카오페이)
+danal(다날휴대폰소액결제)
+danal_tpay(다날일반결제)
+mobilians(모빌리언스 휴대폰소액결제)
+chai(차이 간편결제)
+syrup(시럽페이)
+payco(페이코)
+paypal(페이팔)
+paypal_v2((신)페이팔)
+eximbay(엑심베이)
+naverpay(네이버페이-결제형)
+naverco(네이버페이-주문형)
+smilepay(스마일페이)
+daou(키움페이(구 페이조아))
+paymentwall(페이먼트월)
+eximbay(엑심베이)
+tosspay(토스페이)
+tosspay_v2 ((신)토스페이)
+toss_brandpay(토스페이먼츠 브랜드페이)
+smartro(스마트로)
+smartro_v2((신)스마트로)
+settle(세틀뱅크)
+welcome(웰컴페이먼츠)
+`;
+
+const items = LATEST_PORTONE.split('\n').flatMap((line) => {
+  if (!line) {
+    return [];
+  }
+  const firstQuoteIndex = line.indexOf('(');
+  const key = line.substring(0, firstQuoteIndex);
+  const value = line.substring(firstQuoteIndex + 1);
+  return { key, value: value.replace(')', '') };
+});
+
+items.sort((a, b) => a.key.localeCompare(b.key));
+
+const main = async () => {
+  const options = await prettier.resolveConfig(
+    path.resolve(__dirname, '../.prettierrc.js'),
+  );
+
+  const content = await prettier.format(
+    `
+    /** @see https://developers.portone.io/docs/ko/sdk/javascript-sdk/payrq */
+    export type Pg =
+    | ${items.map((item) => `'${item.key}' // ${item.value}`).join('\n| ')};
+  `,
+    { parser: 'typescript', ...options },
+  );
+
+  fs.writeFileSync(path.resolve(__dirname, '../src/Pg.ts'), content, 'utf-8');
+};
+
+main()
+  .then(() => {
+    console.log('Done ✨');
+  })
+  .catch((e) => {
+    console.error(e);
+  });
--- src/Pg.ts
+++ src/Pg.ts
@@ -1,32 +1,37 @@
 /** @see https://developers.portone.io/docs/ko/sdk/javascript-sdk/payrq */
 export type Pg =
-  | 'html5_inicis' // 이니시스웹표준
-  | 'inicis_unified' // 이니시스 통합인증
-  | 'inicis' // 이니시스 ActiveX결제창 or API 방식
-  | 'kcp' // NHN KCP
-  | 'kcp_billing' // NHN KCP 정기결제
-  | 'uplus' // 토스페이먼츠(구 LG U+)
-  | 'tosspayments' // 토스페이먼츠 신모듈
-  | 'nice' // 나이스페이
-  | 'jtnet' // JTNet
-  | 'kicc' // 한국정보통신
   | 'bluewalnut' // 블루월넛
-  | 'kakaopay' // 카카오페이
+  | 'chai' // 차이 간편결제
   | 'danal' // 다날휴대폰소액결제
   | 'danal_tpay' // 다날일반결제
-  | 'mobilians' // 모빌리언스 휴대폰소액결제
-  | 'chai' // 차이 간편결제
-  | 'syrup' // 시럽페이
-  | 'payco' // 페이코
-  | 'paypal' // 페이팔
-  | 'eximbay' // 엑심베이
-  | 'naverpay' // 네이버페이-결제형
-  | 'naverco' // 네이버페이-주문형
-  | 'smilepay' // 스마일페이
   | 'daou' // 키움페이(구 페이조아)
-  | 'paymentwall' // 페이먼트월
   | 'eximbay' // 엑심베이
-  | 'tosspay' // 토스간편결제
-  | 'smartro' // 스마트로
+  | 'eximbay' // 엑심베이
+  | 'html5_inicis' // 이니시스웹표준
+  | 'inicis' // 이니시스ActiveX결제창 or API 방식
+  | 'inicis_unified' // 이니시스 통합인증
+  | 'jtnet' // JTNet
+  | 'kakaopay' // 카카오페이
+  | 'kcp' // NHN KCP 일반결제창 or API 방식 정기결제
+  | 'kcp_billing' // NHN KCP 정기결제
+  | 'kicc' // 한국정보통신
+  | 'mobilians' // 모빌리언스 휴대폰소액결제
+  | 'naverco' // 네이버페이-주문형
+  | 'naverpay' // 네이버페이-결제형
+  | 'nice' // 나이스페이
+  | 'nice_v2' // (신나이스페이)
+  | 'payco' // 페이코
+  | 'paymentwall' // 페이먼트월
+  | 'paypal' // 페이팔
+  | 'paypal_v2' // (신페이팔)
   | 'settle' // 세틀뱅크
-  | (string & {});
+  | 'smartro' // 스마트로
+  | 'smartro_v2' // (신스마트로)
+  | 'smilepay' // 스마일페이
+  | 'syrup' // 시럽페이
+  | 'toss_brandpay' // 토스페이먼츠 브랜드페이
+  | 'tosspay' // 토스페이
+  | 'tosspay_v2 ' // (신토스페이)
+  | 'tosspayments' // (신토스페이먼츠)
+  | 'uplus' // 토스페이먼츠(구 LG U+)
+  | 'welcome'; // 웰컴페이먼츠;
